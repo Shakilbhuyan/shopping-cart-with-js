9e6df6b97ed0b78b71f8151f7e0198cd4ee0a32c
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-03-29 22:27:17 +0600

    first commit

---
+++ script/phone.js
@@ -0,0 +1,36 @@
+function updatePhonenumber(isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    let newPhoneNumber;
+    if(isIncrease == true){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else{
+newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberField.value = newPhoneNumber; 
+    return newPhoneNumber;
+
+};
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+};
+
+document.getElementById('btn-phone-plus').addEventListener('click', function(){
+   const newPhoneNumber = updatePhonenumber(true);
+   updatePhoneTotalPrice(newPhoneNumber);
+   calculateSubtotal();
+
+  
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', function(){
+  const newPhoneNumber = updatePhonenumber(false);
+  updatePhoneTotalPrice(newPhoneNumber);
+  calculateSubtotal();
+})
